100cd9e0346bfb74df774f125cc4210b3f383e04
akumpula <akumpula01@gmail.com>
2015-05-13 23:22:38 -0500

    Fixed some pricing stuff

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -69,6 +69,6 @@ class ListingsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
-      params.require(:listing).permit(:name, :description, :price, :room_id, :age_id, { :color_ids => [] })
+      params.require(:listing).permit(:name, :description, :Price, :room_id, :age_id, { :color_ids => [] })
     end
 end
--- app/views/listings/show.json.jbuilder
+++ app/views/listings/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @listing, :id, :name, :description, :price, :room_id, :age_id, :created_at, :updated_at
+json.extract! @listing, :id, :name, :description, :Price, :room_id, :age_id, :created_at, :updated_at
